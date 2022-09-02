b726f989244d31e2d5667167e3ad6521eda17165
Noah Johnson <jnoahjohnson@gmail.com>
2022-09-02 11:07:17 -0600

    Add sponsor page

--- app/routes/sponsors.tsx
+++ app/routes/sponsors.tsx
@@ -1,14 +1,12 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import ContentContainer from "~/components/layout/content-container";
-import { getClient } from "~/lib/sanity/get-client";
+
+import { getSponsors } from "~/services/airtable.server";
 
 export const loader: LoaderFunction = async () => {
-  const sponsors = await getClient().fetch(
-    `*[_type == "sponsor"]{ name, logo }`
-  );
+  const sponsors = await getSponsors();
 
-  console.log(sponsors);
   return { sponsors };
 };
 
@@ -18,16 +16,48 @@ export default function SponsorsPage() {
   }>();
 
   return (
-    <ContentContainer>
-      <h1>Sponsors</h1>
-      <ul className="grid grid-cols-3 gap-2">
-        {sponsors.map(({ name, logo }) => (
-          <li key={name}>
-            <img src={logo} alt={name} className="w-full" />
-            <p>{name}</p>
-          </li>
-        ))}
-      </ul>
-    </ContentContainer>
+    <div className="py-6">
+      <ContentContainer>
+        <header className="pb-6">
+          <h1 className="text-4xl font-extrabold">Sponsors</h1>
+          <p className="max-w-xl text-lg text-gray-700">
+            We have incredible sponsors. They support the club activities and
+            students. Throughout the semester there are info sessions with our
+            sponsors to learn more abou the opportunities that they provide
+          </p>
+        </header>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+          {sponsors.map(({ name, logo }) => (
+            <li
+              key={name}
+              className="p-6 bg-gray-100 rounded relative flex flex-col items-center justify-center pb-16 aspect-1"
+            >
+              <img src={logo} alt={name} className="max-h-48 w-auto" />
+
+              <p className="left-4 bottom-4 font-bold text-sm absolute">
+                {name}
+              </p>
+            </li>
+          ))}
+        </ul>
+        <div className="mt-6 rounded w-full bg-gradient-to-br from-blue-500 to-blue-700 text-white p-10">
+          <h2 className="text-2xl font-extrabold mb-1">
+            Interested in becoming a sponsor?
+          </h2>
+          <p className="text-lg mb-4">
+            Sponsorship not only supports the students, but it also provides you
+            with incredible connections through events and info sessions.
+          </p>
+          <a
+            href="https://airtable.com/shrWacVnZuWqgrnHk"
+            className="bg-white px-4 py-2 rounded text-blue-900"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Request Information
+          </a>
+        </div>
+      </ContentContainer>
+    </div>
   );
 }
